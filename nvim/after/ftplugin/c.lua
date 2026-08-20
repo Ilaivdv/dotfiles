a9e15ccefcf5a0d69009b92252a1059c7e962e04
Ilai Van Der Veldt <ivan-der@f0r3s20.codam.nl>
2026-08-20 12:08:11 +0200

    feat(nvim): stop clang lsp from auto-including headers

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -2,10 +2,10 @@ vim.o.tabstop = 4
 vim.opt_local.textwidth = 80
 set_line_limit(80)
 
-vim.lsp.config('clang', {
-	cmd = {'clangd'},
-	filetype = {'c', 'h'},
-	root_markers = {'.clangd', 'compile_commands.json'},
+vim.lsp.config("clang", {
+	cmd = { "clangd", "--header-insertion=never" },
+	filetype = { "c", "h" },
+	root_markers = { ".clangd", "compile_commands.json" },
 })
 
-vim.lsp.enable('clang')
+vim.lsp.enable("clang")
