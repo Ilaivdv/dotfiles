2f5c2926d3422ab8b1c460461921d85421c4d40f
ilaivdv <ilaivanderveldt@gmail.com>
2026-09-13 20:43:02 +0200

    Add mini clues config to get hints on certain key combinations, add backspace keybind for navigating oil

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -30,7 +30,6 @@ vim.pack.add({
 })
 
 require "mason".setup()
-require "oil".setup({ view_options = { show_hidden = true } })
 require "vim._core.ui2".enable({ msg = { targets = "msg" } })
 require "tiny-cmdline".setup()
 require "mini.icons".setup()
@@ -40,7 +39,6 @@ require "mini.diff".setup()
 require "mini.surround".setup()
 require "mini.ai".setup()
 require "mini.comment".setup()
-require "mini.clue".setup()
 require "markview".setup()
 
 -- Show colorcolumn only if cursor is over line limit
@@ -53,6 +51,11 @@ function set_line_limit(max_line)
 	})
 end
 
+require "oil".setup({
+	view_options = { show_hidden = true },
+	keymaps = { ["<BS>"] = { "actions.parent", mode = "n" } }
+})
+
 require "mini.pick".setup({
 	window = {
 		config = {
@@ -64,6 +67,56 @@ require "mini.pick".setup({
 			height = 20,
 		}
 	}
+})
+
+local miniclue = require("mini.clue")
+miniclue.setup({
+  triggers = {
+    -- Leader triggers
+    { mode = { "n", "x" }, keys = "<Leader>" },
+
+    -- `[` and `]` keys
+    { mode = "n", keys = "[" },
+    { mode = "n", keys = "]" },
+
+    -- Built-in completion
+    { mode = "i", keys = "<C-x>" },
+
+    -- `g` key
+    { mode = { "n", "x" }, keys = "g" },
+
+    -- Marks
+    { mode = { "n", "x" }, keys = "'" },
+    { mode = { "n", "x" }, keys = "`" },
+
+    -- Registers
+    { mode = { "n", "x" }, keys = '"' },
+    { mode = { "i", "c" }, keys = "<C-r>" },
+
+    -- Window commands
+    { mode = "n", keys = "<C-w>" },
+
+    -- `z` key
+    { mode = { "n", "x" }, keys = "z" },
+
+	-- `s` key
+    { mode = { "n", "x" }, keys = "s" },
+  },
+
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.square_brackets(),
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
+
+  window = {
+	  delay = 250
+  }
 })
 
 require "mini.statusline".setup({
